1dce9a005fa6fc3ef889de66af1e825d9e5042e0
equeirozdenoronha <equeirozdenoronha@gmail.com>
2019-02-14 21:03:34 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  rect(215,115,90,200);
+  rect(75,50,70,100);
+  rect(85,300,60,90);
+  rect(85,160,60,130);
+  stroke(3);
+	strokeWeight(8);
+  stroke(40);
+  line(180,120,180,85);
+  
+}
